fe54b22948a5ae114d42a9efad5198278565fda9
soundeffects <james@youngbloods.org>
2018-07-16 16:17:46 +0000

    Specifying button conditions

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import _ from 'lodash';
 import courses from '../courses';
 import Course from './course'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      selected: 0
+      selectedCourses: []
     }
     
     this.select = this.select.bind(this);
   }
   
-  select(increment) {
-    this.setState({ selected: this.state.selected + increment });
+  select(course, add) {
+    const selectedCourses = this.state.selectedCourses;
+    if (add) {
+      selectedCourses.push(course);
+    } else {
+      _.pull(selectedCourses, course);
+    }
+    this.setState({ selectedCourses });
   }
   
   render() {
@@ -22,9 +30,10 @@ class App extends React.Component {
       <div className="App">
         <h1>Scheduler</h1>
         <main>
-          { courses.map(course => <Course details={course} select={this.select} selected={this.state.selected}/> ) }
+          {courses.map(course => 
+            <Course details={course} select={this.select} selectedCourses={this.state.selectedCourses}/> 
+          )}
         </main>
-        <h1>{this.state.selected}</h1>
       </div>
     );
   }
--- src/components/course.js
+++ src/components/course.js
@@ -13,16 +13,26 @@ class Course extends React.Component {
   }
   
   handleClick(event) {
-    this.props.select(1);
-    this.setState({ scheduled: true });
-    
+    this.props.select(this.props.details.name, !this.state.scheduled);
+    this.setState({ scheduled: !this.state.scheduled });
+  }
+  
+  conditionalItems() {
+    if(this.state.scheduled || this.props.selectedCourses.length < 6) {
+      if (this.state.scheduled) {
+        return <button className="remove-course" onClick={this.handleClick}>-</button>;
+      } else {
+        return <button className="add-course" onClick={this.handleClick}>+</button>;
+      }
+      
+    }
   }
   
   render() {
     return (
       <article>
         <h2>{this.props.details.name}</h2>
-        { (this.props.selected < 6) ? <button onClick={this.handleClick}>+</button> : null }
+        { this.conditionalItems() }
       </article>
     );
   }
